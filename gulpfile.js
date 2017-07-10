5e968670abf38728976821a3a0432b8483501eb8
rom4uk <rsulyma86@gmail.com>
2017-07-11 02:00:25 +0300

    create npm tasks

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var minifyCSS = require('gulp-csso');
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var browserSync = require('browser-sync').create();
+var gulpSequence = require('gulp-sequence');
+
+
+
+
+
+// add task less, concat
+gulp.task('less', function () {
+	return gulp.src('less/*.less')
+	.pipe(less())
+	.pipe(concat('style.css'))
+	.pipe(gulp.dest('./style'))
+	.pipe(browserSync.stream())
+});
+
+gulp.task('livereload', ['less'], function(){
+	browserSync.init({
+		server: './'
+	})
+	gulp.watch('less/*.less', ['less'])
+	gulp.watch('*.html').on('change', browserSync.reload)
+})
